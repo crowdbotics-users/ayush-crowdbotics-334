184fa483d49d13f0ec930a8484da9a54bcade363
Saurav <sp.gharti@gmail.com>
2018-08-13 18:00:00 +0000

    add package satchless

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'satchless', 'url': 'http://pypi.python.org/pypi/satchless/1.1.3'},
 	{'name':'django-tastypie', 'url': 'http://pypi.python.org/pypi/django-tastypie/0.14.0'},
 	{'name':'django-app-metrics', 'url': 'http://pypi.python.org/pypi/django-app-metrics/0.9.0'},
 	{'name':'django-experiments', 'url': 'http://pypi.python.org/pypi/django-experiments/1.2.0'},
